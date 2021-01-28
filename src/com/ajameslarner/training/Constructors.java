cc815dcb97d7a6bbf8e1dd792f1a7c8e05d23877
ajameslarner <ajameslarner@gmail.com>
2021-01-28 00:16:43 +0000

    enough for one day

--- src/com/ajameslarner/training/Constructors.java
+++ src/com/ajameslarner/training/Constructors.java
@@ -8,7 +8,7 @@ public class Constructors {
     String name;
     String color;
 
-    public Constructors() {
+    public Constructors() {             //The constructor is called when the object is created! it initializes the objects attributes
         age = 10;
         price = 400;
         name = "car";
@@ -17,5 +17,9 @@ public class Constructors {
 
     public static void main(String[] args) {
 
+        Constructors consObjects = new Constructors();
+        System.out.println(consObjects.age);
+
+        //That's enough for today, im drunk...
     }
 }
